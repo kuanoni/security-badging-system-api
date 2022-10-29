0dd3909718b4939bab25863843a630ac71d14bc3
kuanoni <tehgamemaker@gmail.com>
2022-10-29 14:25:54 -0700

    feat: can search for first & last badge owner names

--- src/routes/credentials.routes.js
+++ src/routes/credentials.routes.js
@@ -5,8 +5,36 @@ const basicRoutes = require('./basicRoutes');
 const credentialsRoutes = () => {
 	const router = express.Router();
 
+	// custom RegEx search for badgeOwnerName filter
+	router.get('/get', async (req, res, next) => {
+		const filterValue =
+			req.query.filter === 'badgeOwnerName'
+				? new RegExp('(^|\\s)' + req.query.value?.toLowerCase() + '\\w*', 'gsi')
+				: new RegExp('^' + req.query.value?.toLowerCase(), 'i');
+		const filter = { [req.query.filter]: { $regex: filterValue } };
+		const projection = req.query.props ? req.query.props.replace(',', ' ') : '';
+
+		const page = req.query.page || 1;
+		const limit = req.query.limit || 30;
+
+		try {
+			const data = await model.find(filter, projection, { limit, skip: limit * (page - 1) }).sort(
+				req.query.filter && {
+					[req.query.filter]: 'asc',
+				}
+			);
+
+			const count = await model.find(filter, projection).countDocuments();
+
+			res.json({ documents: data, count });
+		} catch (error) {
+			res.status(400).json({ message: error.message });
+		}
+
+		next();
+	});
+
 	router.post('/post', basicRoutes.Post(model));
-	router.get('/get', basicRoutes.Get(model));
 	router.get('/get/:id', basicRoutes.GetById(model));
 	router.patch('/update/:id', basicRoutes.Update(model));
 	router.delete('/delete/:id', basicRoutes.Delete(model));
