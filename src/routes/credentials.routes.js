11cf086ddd824d624e61c1b31359779c2ae2f48d
kuanoni <tehgamemaker@gmail.com>
2022-10-31 10:13:48 -0700

    feat: added getAvailable route for credentials
    returns credentials with no owner

--- src/routes/credentials.routes.js
+++ src/routes/credentials.routes.js
@@ -34,6 +34,31 @@ const credentialsRoutes = () => {
 		next();
 	});
 
+	router.get('/getAvailable', async (req, res, next) => {
+		const filterValue = new RegExp('^' + req.query.value?.toLowerCase(), 'i');
+		const filter = { [req.query.filter]: { $regex: filterValue }, badgeOwnerId: '', badgeOwnerName: '' };
+		const projection = '_id badgeType';
+
+		const page = req.query.page || 1;
+		const limit = req.query.limit || 30;
+
+		try {
+			const data = await model.find(filter, projection, { limit, skip: limit * (page - 1) }).sort(
+				req.query.filter && {
+					[req.query.filter]: 'asc',
+				}
+			);
+
+			const count = await model.find(filter, projection).countDocuments();
+
+			res.json({ documents: data, count });
+		} catch (error) {
+			res.status(400).json({ message: error.message });
+		}
+
+		next();
+	});
+
 	router.post('/post', basicRoutes.Post(model));
 	router.get('/get/:id', basicRoutes.GetById(model));
 	router.patch('/update/:id', basicRoutes.Update(model));
